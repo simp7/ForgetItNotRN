cf4629e4a78f368ea16c539254dc1213afbbc283
simp7 <JHParkOfficial@protonmail.com>
2023-05-05 19:21:17 +0900

    Upgrade project

--- App.tsx
+++ App.tsx
@@ -2,13 +2,11 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
  * @format
  */
 
-import React, { type PropsWithChildren } from 'react';
+import React from 'react';
+import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -27,11 +25,11 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({ children, title }) => {
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function Section({children, title}: SectionProps): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -47,15 +45,17 @@ const Section: React.FC<
       <Text
         style={[
           styles.sectionDescription,
-          { color: isDarkMode ? Colors.light : Colors.dark },
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
         ]}>
         {children}
       </Text>
     </View>
   );
-};
+}
 
-const App = () => {
+function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -94,7 +94,7 @@ const App = () => {
       </ScrollView>
     </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
   sectionContainer: {
