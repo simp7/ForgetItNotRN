225ec4109089b4f324d647486f2fa3875e1d2c10
simp7 <JHParkOfficial@protonmail.com>
2023-05-06 15:17:15 +0900

    Fix eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,40 @@
-module.exports = {
-  root: true,
-  extends: '@react-native-community',
+export default {
+	env: {
+		es6: true,
+		node: true,
+		jest: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+	],
+	'overrides': [
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+    project: './tsconfig.json',
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	plugins: [
+		'react',
+    'react-hooks',
+		'@typescript-eslint'
+	],
+	rules: {
+		'indent': ['error', 'tab'],
+		'linebreak-style': ['error','unix'],
+		'quotes': [ 'error', 'single'],
+		'semi': ['error', 'always'],
+    'react-native/no-raw-text': ['error'],
+	},
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
