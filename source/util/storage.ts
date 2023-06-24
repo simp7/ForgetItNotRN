5388b52f2e3a45cfbde5c88ff9d192dca4d923df
simp7 <JHParkOfficial@protonmail.com>
2023-06-24 11:24:21 +0900

    Refactor Storage.

--- source/util/storage.ts
+++ source/util/storage.ts
@@ -1,7 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Dayjs } from "dayjs";
 
-import { DEFAULT_SETTING } from "../constant";
+import { DEFAULT_CORE, DEFAULT_SETTING } from "../constant";
 import { CardData, Core } from "../model/cardData";
 import { Setting } from "../model/setting";
 import { formatDate, now } from "./date";
@@ -12,54 +12,22 @@ enum StorageKey {
 	setting = 'SETTING',
 }
 
-export const loadLastOpenedDate = async () => {
-	return AsyncStorage.getItem(StorageKey.lastOpened) ?? "";
-};
-
-export const saveLastOpenedDate = async () => {
-	return AsyncStorage.setItem(StorageKey.lastOpened, formatDate(now()));
-};
-
-const getCardKey = (date: Dayjs) => 'card' + formatDate(date);
-
-export const loadCardData = async (date: Dayjs) => {
-	const raw = await AsyncStorage.getItem(getCardKey(date)) ?? "";
-	return JSON.parse(raw) as CardData[];
-};
-
-export const saveCardData = async (date: Dayjs, data: CardData[]) => {
-	return AsyncStorage.setItem(getCardKey(date), JSON.stringify(data));
-};
-
-// TODO: implement this.
-export const loadCore = async () => {
-	const raw = await AsyncStorage.getItem(StorageKey.core) ?? "";
-
-	return JSON.parse(await AsyncStorage.getItem(StorageKey.core) ?? "") as Core;
-};
-
-// TODO: implement this.
-export const saveCore = async () => {
-
-};
-
-export const loadSetting = async () => {
-	const raw = await AsyncStorage.getItem(StorageKey.setting) ?? "";
-	const setting: Setting = !!raw ?  JSON.parse(raw) : DEFAULT_SETTING;
-	return setting;
-};
-
-export const saveSetting = async (setting: Setting) => {
-	return AsyncStorage.setItem(StorageKey.setting, JSON.stringify(setting));
-};
-
-export const loadItem = async (date: string) => {
-	const raw = await AsyncStorage.getItem(date);
-	const result: CardData[] = raw ? JSON.parse(raw) : [];
+const load = async <T, > (key: string, defaultValue: T) => {
+	const rawData = await AsyncStorage.getItem(key) ?? "";
+	const result: T = !!rawData ? JSON.parse(rawData) : defaultValue;
 	return result;
 };
+const save = async <T, > (key: string, data: T) => AsyncStorage.setItem(key, JSON.stringify(data));
 
-export const saveItem = async (item: CardData) => {
-	const previous = await formatDate(now());
-	return AsyncStorage.setItem(formatDate(now()), JSON.stringify([...previous, item]));
-};
+export const loadLastOpenedDate = async () => load(StorageKey.lastOpened, "");
+export const saveLastOpenedDate = async () => save(StorageKey.lastOpened, formatDate(now()));
+
+const getCardKey = (date: Dayjs) => 'card' + formatDate(date);
+export const loadCardData = async (date: Dayjs) => load<CardData[]>(getCardKey(date), []);
+export const saveCardData = async (date: Dayjs, data: CardData[]) => save(getCardKey(date), data);
+
+export const loadCore = async () => load(StorageKey.core, DEFAULT_CORE);
+export const saveCore = async (core: Core) => save(StorageKey.core, core);
+
+export const loadSetting = async () => load(StorageKey.setting, DEFAULT_SETTING);
+export const saveSetting = async (setting: Setting) => save(StorageKey.setting, setting);
