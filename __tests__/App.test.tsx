c0113fe0c7d9d76cddc58c5b54b8510bbe01f742
simp7 <JHParkOfficial@protonmail.com>
2023-06-24 12:22:36 +0900

    Upgrade react native version to 0.72

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -4,6 +4,8 @@
 
 import 'react-native';
 
+// Note: import explicitly to use the types shiped with jest.
+import { it } from '@jest/globals';
 import React from 'react';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
